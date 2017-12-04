c006650700be564a473028222a238221c0d3c048
Kalle <kalhelm@hotmail.com>
2017-12-04 10:52:31 +0200

    Eka push

---
+++ desktop/src/main/java/uniche/desktop/DesktopLauncher.java
@@ -0,0 +1,12 @@
+package main.java.uniche.desktop;
+
+import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
+import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+import main.java.uniche.MainLauncher;
+
+public class DesktopLauncher {
+	public static void main (String[] arg) {
+		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+		new LwjglApplication(new MainLauncher(), config);
+	}
+}
