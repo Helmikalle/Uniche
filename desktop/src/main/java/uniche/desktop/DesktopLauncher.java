777071ba2af25483d8e839856b3512bb1c2f6fdd
Kalle <kalhelm@hotmail.com>
2017-12-06 01:32:20 +0200

    Vipellystä rauhotettu ja refactoroitu koodia

--- desktop/src/main/java/uniche/desktop/DesktopLauncher.java
+++ desktop/src/main/java/uniche/desktop/DesktopLauncher.java
@@ -7,9 +7,12 @@ import main.java.uniche.MainLauncher;
 public class DesktopLauncher {
 	public static void main (String[] arg) {
 		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
-		config.title = "Drop";
+		//Ikkunan confeja määritelty
+		config.title = "UNICHE";
 		config.width = 800;
 		config.height = 480;
+		config.foregroundFPS = 60;
+		config.backgroundFPS = 60;
 		new LwjglApplication(new MainLauncher(), config);
 	}
 }
